a2e5c0acaafcacf0fe7532ca2b29a06c59c19a44
RafaaDomingues <rafaellycaroline199@gmail.com>
2025-09-05 20:08:33 +0000

    coordenadas

---
+++ app.js
@@ -0,0 +1,35 @@
+const trilhaAventura = [
+    { nome: "Fazenda Monte Bello", lat: -23.219201, lng: -49.759417 },
+    { nome: "Ponte Pênsil", lat: -23.098811, lng: -49.742955 },
+    { nome: "Morro do Gavião", lat: -23.241006, lng: -49.695877 },
+    { nome: "Pedra do Índio", lat: -23.228086, lng: -49.678255 },
+    { nome: "Prainha Camping", lat: -23.262455, lng: -49.700248 }
+];
+
+const trilhaHospedagem = [
+    { nome: "Tayaya", lat: -23.195125, lng: -49.683385 },
+    { nome: "Daj Resort", lat: -23.295761, lng: -49.736630 },
+    { nome: "Pousada Victor", lat: -23.197607, lng: -49.757993 },
+    { nome: "Pousada Ruvina", lat: -23.280173, lng: -49.806206 },
+    { nome: "Ilha do Bêca", lat: -23.186667, lng: -49.684243 },
+    { nome: "Ilha 54", lat: -23.168182, lng: -49.683743 },
+    { nome: "Villa Bueno", lat: -23.183964, lng: -49.683397 },
+    { nome: "Rancho da Colina", lat: -23.180217, lng: -49.693027 },
+    { nome: "Marina Angra", lat: -23.183418, lng: -49.700253 },
+    { nome: "Fazenda Pinhalzinho", lat: -23.164416, lng: -49.750124 },
+    { nome: "Villa Nova", lat: -23.198247, lng: -49.755459 },
+    { nome: "Fazenda Platina", lat: -23.219241, lng: -49.802912 }
+];
+
+const trilhaGastronomia = [
+    { nome: "Pedra do Índio", lat: -23.22925864786942, lng: -49.67692854604166 },
+    { nome: "Blue Moon", lat: -23.19668601903206, lng: -49.757500485562 },
+    { nome: "Ki-delícia", lat: -23.191686018703877, lng: -49.7577207178735 },
+    { nome: "Tucão", lat: -23.196665975175157, lng: -49.754784204299196 },
+    { nome: "Magnata", lat: -23.198361503991475, lng: -49.757240361971014 },
+    { nome: "Divino Sabor", lat: -23.19475139919245, lng: -49.75811871916535 },
+    { nome: "Chef Adriano", lat: -23.19862807608063, lng: -49.75339923498804 },
+    { nome: "Recanto da Cascata", lat: -23.187519684390224, lng: -49.71055479265805 },
+    { nome: "Parada da Serra", lat: -23.27476717000394, lng: -49.75163014398363 },
+    { nome: "Queijaria Bela Vista", lat: -23.33101479632104, lng: -49.8360618331078 }
+];
