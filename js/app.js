7520c27328db3353bbaea700dd1a355a26743eb1
Nilson <nilsongithub@gmail.com>
2025-09-25 20:30:03 -0300

    update

--- js/app.js
+++ js/app.js
@@ -34,24 +34,51 @@ const trilhaGastronomia = [
     { nome: "Queijaria Bela Vista", lat: -23.33101479632104, lng: -49.8360618331078 }
 ];
 
+const gastronomiaIcon = L.icon({
+    iconUrl: 'asset/img/icon-gastronomia.png',
+    iconSize: [30, 35],         // largura x altura
+    iconAnchor: [9, 18],          // base central do ícone
+    popupAnchor: [0, -18]          // popup acima do ícone
+});
 
-function horaAventura(tipo){
+
+const hospedagemIcon = L.icon({
+    iconUrl: 'asset/img/icon-hospedagem.png',
+    iconSize: [30, 35],         // largura x altura
+    iconAnchor: [9, 18],          // base central do ícone
+    popupAnchor: [0, -18]          // popup acima do ícone
+});
+
+const aventuraIcon = L.icon({
+    iconUrl: 'asset/img/icon-aventura.png',
+    iconSize: [30, 35],         // largura x altura
+    iconAnchor: [9, 18],          // base central do ícone
+    popupAnchor: [0, -18]          // popup acima do ícone
+});
+
+const rota = document.getElementById("rota")
+rota.addEventListener("change", () => {
+    const tipoSelecionado = rota.value;
+    horaAventura(tipoSelecionado);
+});
+
+function horaAventura(tipo) {
     switch (tipo) {
-  case "trilhaAventura":
-    console.log("Hoje é segunda-feira.");
-    break;
-  case "TrilhaHospedagem":
-    console.log("Hoje é terça-feira.");
-    break;
-  case "TrilhaGastronomia":
-    addGastronomiaMaker();
-    break;
+        case "TrilhaAventura":
+            addMaker(trilhaAventura, iconf = aventuraIcon);
+            break;
+        case "TrilhaHospedagem":
+            addMaker(trilhaHospedagem, iconf = hospedagemIcon);
+            break;
+        case "TrilhaGastronomia":
+            addMaker(trilhaGastronomia, iconf = gastronomiaIcon);
+            break;
+    }
 }
-}
 
 
 
-const map = L.map('map' ).setView([-23.1939, -49.7578],15) 
+const map = L.map('map').setView([-23.1939, -49.7578], 15)
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -59,65 +86,54 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
-const gastronomiaIcon = L.icon({
-  iconUrl: 'asset/img/icon-gastronomia.png',
-  iconSize:     [30, 35],         // largura x altura
-  iconAnchor:   [9, 18],          // base central do ícone
-  popupAnchor:  [0, -18]          // popup acima do ícone
-});
 
-
-
-function addGastronomiaMaker(){
-    lista =[]
-    for(let i = 0; i < trilhaGastronomia.length; i++) {
-     var x = L.marker([trilhaGastronomia[i].lat,trilhaGastronomia[i].lng],{ icon: gastronomiaIcon }).addTo(map);
+function addMaker(trilha, iconf){ 
+    lista = [] 
+    limparMarcadores()
+    for (let i = 0; i < trilha.length; i++) { 
+        var x = L.marker([trilha[i].lat, trilha[i].lng], { icon: iconf }).addTo(map); 
         x.bindPopup(`
-        //   <b>${trilhaGastronomia[i].nome}</b><br>
-        //   <a href="https://ribeiraoclaro.pr.gov.br/" target="_blank">Saiba mais</a>
-        // `).openPopup();
-        lista.push([trilhaGastronomia[i].lat,trilhaGastronomia[i].lng])
-    }
-    mapear(lista)
+             // <b>${trilha[i].nome}</b><br> // <a href="https://ribeiraoclaro.pr.gov.br/" target="_blank">Saiba mais</a> // `).openPopup(); 
+             lista.push([trilha[i].lat, trilha[i].lng]) } 
+             mapear(lista) } 
+
+function limparMarcadores() {
+    map.eachLayer(function(layer) {
+        if (layer instanceof L.Marker) {
+            map.removeLayer(layer);
+        }
+    });
 }
+             
+let rotaControls = []; // guarda todas as rotas para poder limpar depois
 
-
+function limparRotas() {
+    rotaControls.forEach(r => map.removeControl(r));
+    rotaControls = [];
+}
 
 function mapear(lista) {
-    const waypoints = lista.map(coord => L.latLng(coord[0], coord[1]));
-    L.Routing.control({
-        waypoints: waypoints,
-        createMarker: () => null,
-        lineOptions: {
-            styles: [{ color: 'red', weight: 4 }]
-        },
-        addWaypoints: false,
-        routeWhileDragging: false,
-        draggableWaypoints: false,
-        show: false
-    }).addTo(map);
-}
+    limparRotas();
+    for (let i = 0; i < lista.length - 1; i++) {
+        const waypoints = [
+            L.latLng(lista[i][0], lista[i][1]),
+            L.latLng(lista[i+1][0], lista[i+1][1])
+        ];
 
+        const rota = L.Routing.control({
+            waypoints: waypoints,
+            createMarker: () => null,
+            lineOptions: {
+                styles: [{ color: 'red', weight: 4 }]
+            },
+            addWaypoints: false,
+            routeWhileDragging: false,
+            draggableWaypoints: false,
+            show: false
+        }).addTo(map);
 
-horaAventura('TrilhaGastronomia')
-
-// pontoA.bindPopup(`
-//   <b>Cascata</b><br>
-//   <a href="https://ribeiraoclaro.pr.gov.br/" target="_blank">Saiba mais</a>
-// `).openPopup();
-
-
-// var popup = L.popup();
-
-// function onMapClick(e) {
-//     popup
-//         .setLatLng(e.latlng)
-//         .setContent("Anota as cordenadas: " + e.latlng.toString())
-//         .openOn(map);
-// }
-
-// map.on('click', onMapClick);
-
+        // rotaControls.push(rota);
+    }}
 
 
 
