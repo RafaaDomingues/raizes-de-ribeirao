c5b9a4a8b780974bc07c686a4df65064f7d6898d
Rafaa DOmingues <domingues.rafaelly@escola.pr.gov.br>
2025-10-13 21:29:17 -0300

    update

--- js/app.js
+++ js/app.js
@@ -8,11 +8,11 @@ const trilhaAventura = [
 
 const trilhaHospedagem = [
     { nome: "Tayaya", lat: -23.195125, lng: -49.683385 },
-    { nome: "Daj Resort", lat: -23.295761, lng: -49.736630 },
+    { nome: "Daj Resort", lat: -23.295761, lng: -49.736630, ishtml:'daj.html' },
     { nome: "Pousada Victor", lat: -23.197607, lng: -49.757993 },
     { nome: "Pousada Ruvina", lat: -23.280173, lng: -49.806206 },
     { nome: "Ilha do Bêca", lat: -23.186667, lng: -49.684243 },
-    { nome: "Ilha 54", lat: -23.168182, lng: -49.683743 },
+    { nome: "Ilha 54", lat: -23.168182, lng: -49.683743, ishtml:'ilha54.html' },
     { nome: "Villa Bueno", lat: -23.183964, lng: -49.683397 },
     { nome: "Rancho da Colina", lat: -23.180217, lng: -49.693027 },
     { nome: "Marina Angra", lat: -23.183418, lng: -49.700253 },
@@ -23,7 +23,7 @@ const trilhaHospedagem = [
 
 const trilhaGastronomia = [
     { nome: "Pedra do Índio", lat: -23.22925864786942, lng: -49.67692854604166 },
-    { nome: "Blue Moon", lat: -23.19668601903206, lng: -49.757500485562 },
+    { nome: "Blue Moon", lat: -23.19668601903206, lng: -49.757500485562, ishtml:'bluemoon.html' },
     { nome: "Ki-delícia", lat: -23.191686018703877, lng: -49.7577207178735 },
     { nome: "Tucão", lat: -23.196665975175157, lng: -49.754784204299196 },
     { nome: "Magnata", lat: -23.198361503991475, lng: -49.757240361971014 },
@@ -31,7 +31,7 @@ const trilhaGastronomia = [
     { nome: "Chef Adriano", lat: -23.19862807608063, lng: -49.75339923498804,ishtml:'chefadriano.html' },
     { nome: "Recanto da Cascata", lat: -23.187519684390224, lng: -49.71055479265805 },
     { nome: "Parada da Serra", lat: -23.27476717000394, lng: -49.75163014398363 },
-    { nome: "Queijaria Bela Vista", lat: -23.33101479632104, lng: -49.8360618331078 }
+    { nome: "Queijaria Bela Vista", lat: -23.33101479632104, lng: -49.8360618331078, ishtml:'queijaria.html' }
 ];
 
 const gastronomiaIcon = L.icon({
